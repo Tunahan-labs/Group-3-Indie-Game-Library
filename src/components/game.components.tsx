552f974ff52439ded20c634be390664f2d81c033
Tunahan-labs <tunahankursun369@gmail.com>
2025-12-09 13:16:42 +0100

    added the photos

--- src/components/game.components.tsx
+++ src/components/game.components.tsx
@@ -1,9 +1,53 @@
+import cupheadImage from "../assets/cuphead.jpg";
+import hollowKnightImage from "../assets/hollow_knight.jpg";
+import celesteImage from "../assets/celeste.jpg";
+import stardewValleyImage from "../assets/stardew_valley.jpg";
+import undertaleImage from "../assets/undertale.jpg";
+import deadCellsImage from "../assets/dead_cells.jpg";
+
 export const products: Product[] = [
-  { id: 1, name: "Hollow Knight", genre: "Metroidvania", platform: "PC" },
-  { id: 2, name: "Celeste", genre: "Platformer", platform: "PC" },
-  { id: 3, name: "Stardew Valley", genre: "Simulation", platform: "PC" },
-  { id: 4, name: "Undertale", genre: "RPG", platform: "PC" },
-  { id: 5, name: "Dead Cells", genre: "Metroidvania", platform: "PC" },
+  {
+    id: 1,
+    name: "Hollow Knight",
+    genre: "Metroidvania",
+    platform: "PC",
+    image: hollowKnightImage,
+  },
+  {
+    id: 2,
+    name: "Celeste",
+    genre: "Platformer",
+    platform: "PC",
+    image: celesteImage,
+  },
+  {
+    id: 3,
+    name: "Stardew Valley",
+    genre: "Simulation",
+    platform: "PC",
+    image: stardewValleyImage,
+  },
+  {
+    id: 4,
+    name: "Undertale",
+    genre: "RPG",
+    platform: "PC",
+    image: undertaleImage,
+  },
+  {
+    id: 5,
+    name: "Dead Cells",
+    genre: "Metroidvania",
+    platform: "PC",
+    image: deadCellsImage,
+  },
+  {
+    id: 6,
+    name: "Cuphead",
+    genre: "Run and Gun",
+    platform: "PC",
+    image: cupheadImage,
+  },
 ];
 
 export interface Product {
@@ -11,4 +55,5 @@ export interface Product {
   name: string;
   genre: string;
   platform: string;
+  image: string;
 }
